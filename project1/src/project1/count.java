753c5a385cd09ece14455929004537644292e660
sindhureddykancharla <sindoora.kancharla@cuw.edu>
2016-02-10 12:21:50 -0600

    corrected

--- project1/src/project1/count.java
+++ project1/src/project1/count.java
@@ -8,7 +8,7 @@ public class count
 				int count = 0;
 						for(int i = 1; i <=1000000 ; i=i+1)
 					{
-							if((i % 9 == 0) && (i % 13 == 0) &&( i% 27==0) && (i%81==0))
+							if((i % 9 == 0) || (i % 13 == 0) ||( i% 27==0) || (i%81==0))
 						{
 								count = count + 1;
 								
