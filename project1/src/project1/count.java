61f05e8f3e76f41058e70d040ff447e043976118
sindhureddykancharla <sindoora.kancharla@cuw.edu>
2016-02-10 09:44:24 -0600

    initial commit

---
+++ project1/src/project1/count.java
@@ -0,0 +1,21 @@
+package project1;
+
+public class count
+{
+
+			public static void main(String[] args)
+			{
+				int count = 0;
+						for(int i = 1; i <=1000000 ; i=i+1)
+					{
+							if((i % 9 == 0) && (i % 13 == 0) &&( i% 27==0) && (i%81==0))
+						{
+								count = count + 1;
+								
+							}
+					}	
+						System.out.println(count);
+
+			}	
+						}
+
